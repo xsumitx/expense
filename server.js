6e1d0ad9e5bad39ec99a84a3aa77e6fb5367b3ea
xsumitx <127008291+xsumitx@users.noreply.github.com>
2024-01-08 22:25:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.SQL_SEVER_DB, process.env.DBUSER, process.env.SQL_SEVER_PASSWORD, {
-    host: process.env.SQL_SERVER_HOST,
+    host: process.env.SQL_SEVER_HOST,
     dialect:'mysql' 
   });
   module.exports=sequelize;
